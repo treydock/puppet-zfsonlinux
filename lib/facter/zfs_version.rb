949f3a9fd5dfb569ad26595878ffcaf9bb04ac78
Trey Dockendorf <treydock@tamu.edu>
2013-05-08 03:05:33 -0500

    - Remove Linux specific zfs_version as the original code still worked

--- lib/facter/zfs_version.rb
+++ lib/facter/zfs_version.rb
@@ -1,17 +1,6 @@
 require 'facter'
 
 Facter.add('zfs_version') do
-  confine :kernel => "Linux"
-  setcode do
-    if Facter::Util::Resolution.which('dmesg')
-      zfs_v = Facter::Util::Resolution.exec('dmesg | grep ZFS:')
-      zfs_v_match = zfs_v.match(/ZFS filesystem version (\d+)./) unless zfs_v.nil?
-      zfs_version = zfs_v_match.captures.first unless zfs_v_match.nil?
-    end
-  end
-end
-
-Facter.add('zfs_version') do
   setcode do
     if Facter::Util::Resolution.which('zfs')
       zfs_v = Facter::Util::Resolution.exec('zfs upgrade -v')
