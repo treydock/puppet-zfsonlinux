6954bb9016ca82fc6bdd75f359f178c0a1987f62
Trey Dockendorf <treydock@gmail.com>
2013-05-20 21:38:50 -0500

    - Add system tests for SL6 using fork of rspec-system

--- spec/spec_helper_system.rb
+++ spec/spec_helper_system.rb
@@ -1,25 +1,6 @@
 require 'rspec-system/spec_helper'
 require 'rspec-system-puppet/helpers'
 
-module RSpecSystem
-  class NodeSet::Vagrant
-    def teardown
-       log.info "[Vagrant#teardown] closing all ssh channels"
-       RSpec.configuration.ssh_channels.each do |k,v|
-         v.close unless v.closed?
-       end
-
-       if ENV['DESTROY'] =~ /(no|false)/
-         log.info "[Vagrant#teardown] SKIPPING 'vagrant destroy'"
-       else
-         log.info "[Vagrant#teardown] running 'vagrant destroy'"
-         vagrant("destroy --force") 
-       end
-       nil
-     end
-   end
- end
-
 RSpec.configure do |c|
   # Project root for the this module's code
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
