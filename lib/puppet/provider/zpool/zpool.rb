0105f7d9774a86ed827cb64f0371d291d5f6193f
Trey Dockendorf <treydock@gmail.com>
2013-05-24 17:52:02 -0500

    - Set full path for zpool in hopes it allows Puppet 2.6.x to pass in travis-ci tests

--- lib/puppet/provider/zpool/zpool.rb
+++ lib/puppet/provider/zpool/zpool.rb
@@ -1,7 +1,7 @@
 Puppet::Type.type(:zpool).provide(:zpool) do
   desc "Provider for zpool."
 
-  commands :zpool => 'zpool'
+  commands :zpool => '/sbin/zpool'
 
   #NAME    SIZE  ALLOC   FREE    CAP  HEALTH  ALTROOT
   def self.instances
