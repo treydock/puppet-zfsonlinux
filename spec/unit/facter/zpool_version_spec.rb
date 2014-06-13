d2397c15aa6eda0f6561f93b71ece675d4569f11
Trey Dockendorf <treydock@gmail.com>
2014-06-13 13:55:49 -0500

    * Minor tweaks to unit tests for zpool_version fact * Add items to  .gitignore

--- spec/unit/facter/zpool_version_spec.rb
+++ spec/unit/facter/zpool_version_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe 'zpool_version fact' do
 
   before :each do
-    Facter::Util::Resolution.stubs(:which).with("zpool").returns("/usr/bin/zpool")
+    Facter.fact(:kernel).stubs(:value).returns("Linux")
+    Facter::Util::Resolution.stubs(:which).with("zpool").returns("/sbin/zpool")
   end
 
   it "should return correct version on Linux with zfsonlinux" do
-    Facter.fact(:kernel).stubs(:value).returns("Linux")
-    Facter::Util::Resolution.stubs(:exec).with("zpool upgrade -v").returns(my_fixture_read('zfsonlinux_0.6.1'))
+    Facter::Util::Resolution.stubs(:exec).with("zpool upgrade -v").returns(my_fixture_read('zpool_upgrade_v_zfsonlinux'))
     Facter.fact(:zpool_version).value.should == "28"
   end
 end
